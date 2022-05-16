3e57689d4725d0e8146aaa7a2c508c9aca450c67
Otchy <id+github@otchy.net>
2022-05-16 22:50:56 +0900

    0.2.2: Rewrite tests by using snapshots

--- src/ScalableDots.test.ts
+++ src/ScalableDots.test.ts
@@ -8,9 +8,7 @@ describe('scalebleDots', () => {
     const imageData = { width: 2, height: 2, data };
 
     const dots = scalableDots({ imageData, type: 'SQUARE', size: 16, gap: 2 });
-    expect(dots.toMinifiedXml()).toBe(
-      '<svg viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" version="1.1"><path d="m0 0h16v16h-16z" fill="#ff0000"/><path d="m18 0h16v16h-16z" fill="#00c000"/><path d="m0 18h16v16h-16z" fill="#0000ff"/><path d="m18 18h16v16h-16z" fill="#c0c000"/></svg>'
-    );
+    expect(dots.toPrettyXml()).toMatchSnapshot();
   });
   it('renders half-transparent case properly', () => {
     const data = new Uint8ClampedArray([
@@ -19,9 +17,7 @@ describe('scalebleDots', () => {
     const imageData = { width: 2, height: 2, data };
 
     const dots = scalableDots({ imageData, type: 'SQUARE', size: 16, gap: 2 });
-    expect(dots.toMinifiedXml()).toBe(
-      '<svg viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" version="1.1"><path d="m0 0h16v16h-16z" fill="rgba(255,0,0,.5)"/><path d="m18 0h16v16h-16z" fill="rgba(0,192,0,.5)"/><path d="m0 18h16v16h-16z" fill="rgba(0,0,255,.5)"/><path d="m18 18h16v16h-16z" fill="rgba(192,192,0,.5)"/></svg>'
-    );
+    expect(dots.toPrettyXml()).toMatchSnapshot();
   });
   it('renders transparent case properly', () => {
     const data = new Uint8ClampedArray([
@@ -30,12 +26,10 @@ describe('scalebleDots', () => {
     const imageData = { width: 2, height: 2, data };
 
     const dots = scalableDots({ imageData, type: 'SQUARE', size: 16, gap: 2 });
-    expect(dots.toMinifiedXml()).toBe(
-      '<svg viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" version="1.1"><path d="m18 0h16v16h-16z" fill="rgba(0,192,0,.5)"/><path d="m0 18h16v16h-16z" fill="#0000ff"/></svg>'
-    );
+    expect(dots.toPrettyXml()).toMatchSnapshot();
   });
 
-  describe('toPrettyXml', () => {
+  describe('toMinifiedXml', () => {
     it("doesn't throw error", () => {
       const data = new Uint8ClampedArray([
         255, 0, 0, 255, 0, 192, 0, 255, 0, 0, 255, 255, 192, 192, 0, 255,
@@ -49,7 +43,7 @@ describe('scalebleDots', () => {
         gap: 2,
       });
       expect(() => {
-        dots.toPrettyXml();
+        dots.toMinifiedXml();
       }).not.toThrowError();
     });
   });
--- src/SvgDocument.test.ts
+++ src/SvgDocument.test.ts
@@ -4,12 +4,8 @@ import { DotProps, DotType } from './types';
 describe('SvgDocument', () => {
   it('renders empty svg properly', () => {
     const svg = new SvgDocument({ width: 50, height: 100 });
-    expect(svg.toMinifiedXml()).toBe(
-      '<svg viewBox="0 0 50 100" xmlns="http://www.w3.org/2000/svg" version="1.1"/>'
-    );
-    expect(svg.toPrettyXml()).toBe(
-      '<svg viewBox="0 0 50 100" xmlns="http://www.w3.org/2000/svg" version="1.1"/>'
-    );
+    expect(svg.toMinifiedXml()).toMatchSnapshot();
+    expect(svg.toPrettyXml()).toMatchSnapshot();
   });
   describe('addSquareDot', () => {
     it('works', () => {
@@ -18,9 +14,7 @@ describe('SvgDocument', () => {
       svg.addSquareDot({ x: 0, y: 10, size: 10, fill: 'green' });
       svg.addSquareDot({ x: 10, y: 0, size: 10, fill: 'blue' });
       svg.addSquareDot({ x: 10, y: 10, size: 10, fill: 'yellow' });
-      expect(svg.toMinifiedXml()).toBe(
-        '<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" version="1.1"><path d="m0 0h10v10h-10z" fill="red"/><path d="m0 10h10v10h-10z" fill="green"/><path d="m10 0h10v10h-10z" fill="blue"/><path d="m10 10h10v10h-10z" fill="yellow"/></svg>'
-      );
+      expect(svg.toPrettyXml()).toMatchSnapshot();
     });
   });
   describe('addCircleDot', () => {
@@ -30,9 +24,7 @@ describe('SvgDocument', () => {
       svg.addCircleDot({ x: 0, y: 10, size: 10, fill: 'green' });
       svg.addCircleDot({ x: 10, y: 0, size: 10, fill: 'blue' });
       svg.addCircleDot({ x: 10, y: 10, size: 10, fill: 'yellow' });
-      expect(svg.toMinifiedXml()).toBe(
-        '<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" version="1.1"><circle cx="5" cy="5" r="5" fill="red"/><circle cx="5" cy="15" r="5" fill="green"/><circle cx="15" cy="5" r="5" fill="blue"/><circle cx="15" cy="15" r="5" fill="yellow"/></svg>'
-      );
+      expect(svg.toPrettyXml()).toMatchSnapshot();
     });
   });
   describe('addRhombusDot', () => {
@@ -42,9 +34,7 @@ describe('SvgDocument', () => {
       svg.addRhombusDot({ x: 0, y: 10, size: 10, fill: 'green' });
       svg.addRhombusDot({ x: 10, y: 0, size: 10, fill: 'blue' });
       svg.addRhombusDot({ x: 10, y: 10, size: 10, fill: 'yellow' });
-      expect(svg.toMinifiedXml()).toBe(
-        '<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" version="1.1"><path d="m0 5l5 -5l5 5l-5 5z" fill="red"/><path d="m0 15l5 -5l5 5l-5 5z" fill="green"/><path d="m10 5l5 -5l5 5l-5 5z" fill="blue"/><path d="m10 15l5 -5l5 5l-5 5z" fill="yellow"/></svg>'
-      );
+      expect(svg.toPrettyXml()).toMatchSnapshot();
     });
   });
   describe('addDot', () => {
